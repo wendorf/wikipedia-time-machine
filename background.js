5a641a07719bb2df9804656facb919591b7ea7e2
Dan Wendorf <github@danwendorf.com>
2017-10-17 18:09:31 -0700

    Add Wayback Machine as a time machine type

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 let state = null;
 
-chrome.storage.sync.get({travelDate: '2010-01-01', travelOn: false}, function (result) {
+chrome.storage.sync.get({travelDate: '2010-01-01', travelOn: false, travelMachine: "wikipedia"}, function (result) {
   state = result;
 });
 
@@ -12,13 +12,14 @@ chrome.storage.onChanged.addListener((changes) => {
   if (changes.travelOn) {
     state.travelOn = changes.travelOn.newValue;
   }
+
+  if (changes.travelMachine) {
+    state.travelMachine = changes.travelMachine.newValue;
+  }
 });
 
-chrome.webRequest.onBeforeRequest.addListener((details) => {
-    if (!state.travelOn) {
-      return {};
-    }
-
+let travelMachines = {
+  wikipedia: function(details) {
     let url = new URL(details.url);
     if (!url.hostname.match(/^(.+\.|)wikipedia\.org/) || !url.pathname.match(/^\/wiki\//)) {
       return {};
@@ -32,21 +33,49 @@ chrome.webRequest.onBeforeRequest.addListener((details) => {
 
     let timestamp = new Date(state.travelDate).toISOString();
     let queryURL = `https://${url.hostname}/w/api.php?action=query&prop=revisions&format=json&titles=${article}&rvstart=${timestamp}&rvdir=older&rvlimit=1`;
-    let request = new XMLHttpRequest();
-    request.open('GET', queryURL, false);
-    request.send(null);
 
-    let data = JSON.parse(request.responseText);
+    let data = makeRequest(queryURL);
     let revid = Object.values(data.query.pages)[0].revisions[0].revid;
     return {
       redirectUrl: `https://${url.hostname}/w/index.php?title=${article}&oldid=${revid}`
     };
   },
+  wayback: function(details) {
+    let timestamp = new Date(state.travelDate).toISOString().replace(/T.*$/, "").replace(/-/g, "");
+    let queryURL = `https://archive.org/wayback/available?url=${details.url}&timestamp=${timestamp}`;
+
+    let closest = makeRequest(queryURL).archived_snapshots.closest;
+
+    if (!closest) {
+      return {};
+    }
+
+    return {
+      redirectUrl: closest.url
+    };
+  }
+};
+
+chrome.webRequest.onBeforeRequest.addListener((details) => {
+    if (!state.travelOn) {
+      return {};
+    }
+
+    return travelMachines[state.travelMachine](details);
+  },
   {
     urls: [
-      "*://*.wikipedia.org/wiki/*"
+      "<all_urls>"
     ],
     types: ["main_frame"]
   },
   ["blocking"]
 );
+
+function makeRequest(queryURL) {
+  let request = new XMLHttpRequest();
+  request.open('GET', queryURL, false);
+  request.send(null);
+
+  return JSON.parse(request.responseText);
+}
--- popup.js
+++ popup.js
@@ -5,11 +5,16 @@ document.addEventListener('DOMContentLoaded', () => {
   elements = {
     travelOn: document.getElementById('travel-on'),
     travelDate: document.getElementById('travel-date'),
-    travelDateRow: document.getElementById('travel-date-row')
+    travelMachine: document.getElementById('travel-machine'),
   };
 
-  chrome.storage.sync.get({travelDate: '2010-01-01', travelOn: false}, function (result) {
+  chrome.storage.sync.get({travelDate: '2010-01-01', travelOn: false, travelMachine: "wikipedia"}, function (result) {
     state = result;
+    syncUI();
+  });
+
+  elements.travelOn.addEventListener('change', () => {
+    syncState();
     syncUI();
   });
 
@@ -17,16 +22,16 @@ document.addEventListener('DOMContentLoaded', () => {
     syncState();
   });
 
-  elements.travelOn.addEventListener('change', () => {
+  elements.travelMachine.addEventListener('change', () => {
     syncState();
-    syncUI();
-  });
+  })
 });
 
 function syncState() {
   state = {
     travelDate: elements.travelDate.value,
-    travelOn: elements.travelOn.checked
+    travelOn: elements.travelOn.checked,
+    travelMachine: elements.travelMachine.options[elements.travelMachine.options.selectedIndex].value
   };
   chrome.storage.sync.set(state);
 }
@@ -37,9 +42,15 @@ function syncUI() {
   elements.travelDate.value = state.travelDate;
   elements.travelDate.disabled = !state.travelOn;
 
-  if (state.travelOn) {
-    elements.travelDateRow.classList.remove("disabled");
-  } else {
-    elements.travelDateRow.classList.add("disabled");
-  }
+  elements.travelMachine.disabled = !state.travelOn;
+
+  Array.from(document.getElementsByClassName('travel-row')).forEach((row) => {
+    if (state.travelOn) {
+      row.classList.remove("disabled");
+    } else {
+      row.classList.add("disabled");
+    }
+  });
+
+  Array.from(elements.travelMachine.options).find((o) => o.value === state.travelMachine).selected = true
 }
